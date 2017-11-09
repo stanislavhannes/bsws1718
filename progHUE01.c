78efbf05894cf220a5da85dcacf5719a2447af39
Josu <josua.loh@mni.thm.de>
2017-11-09 18:23:26 +0100

    Eingabe funktioniert, alles weitere in der .c

--- progHUE01.c
+++ progHUE01.c
@@ -4,154 +4,82 @@
 
 #include "progHUE01.h"
 
-char *eingabe = NULL;
-size_t length;
-int inputLength;
+char eingabe[500];
 
-struct befehl {
+typedef struct {
 	char *programmname;
-	char *argumente;
-};
+	char *argumente[19];
+} Befehl;
 
-//struct befehl befehle[10];
+Befehl befehle[10];
+
+// am Wochenende:
+// Kontrolle kritischer Eingaben, zB a b; ; c
+// eventuell Optimierung von strtok
+
+// Leereichen am Ende des Strings, welches durch die Eingabe-Taste geschrieben
+// wird nach der Eingabe, aus der Eingabe (eingabe) entfernen
+
+// Überprüfen von i, j und k in den Schleifen
 
 int main() {
 	while(1) {
 
 		printf("> ");
 
-	  inputLength = getline(&eingabe, &length, stdin);
+		if(fgets(eingabe, sizeof(eingabe), stdin) == NULL) {exit(0);}
 
-		if (inputLength == 1) {continue;}
-		else if (inputLength > 501) {
-			printf("Ihr Eingabe ist zu groß\n");
-			continue;
+
+		char *tempbefehle[10];
+		char delimiter[] = ";";
+		int i = 0;
+
+		tempbefehle[i] = strtok(eingabe, delimiter);
+
+		while(tempbefehle[i] != NULL && i < 10) {
+			i++;
+			tempbefehle[i] = strtok(NULL, delimiter);
 		}
 
-		int correct = prepareEingabe();
+		for (int j = 0; j < i; j++) {
+			char delimiterTwo[] = " ";
+			char *temp;
+			int k = 0;
 
-		if (correct == -1) {
-			printf("Zu viele Befehle!\n");
-			continue;
+			temp = strtok(tempbefehle[j], delimiterTwo);
+			befehle[j].programmname = temp;
+
+			while(temp != NULL && k < 19) {
+				temp = strtok(NULL, delimiterTwo);
+				befehle[j].argumente[k] = temp;
+				k++;
+			}
 		}
 
-		if (eingabe[0] == ';' && eingabe[1] == '\n') {continue;}
-
-		splitEingabe();
-
-		printf("%s", eingabe);
-		printf("%d\n", inputLength);
-		inputLength = 0;
-		eingabe = NULL;
+		// schreibt die befehle auf der Konsole
+		test();
 	}
 
 	return 0;
+}
+
+void test() {
+	for (int i=0; i < 10; i++) {
+		if (befehle[i].programmname != NULL) {printf("%s\n", befehle[i].programmname);}
+
+		for (int j=0; j < 19; j++) {
+			if (befehle[i].argumente[j] != NULL) {printf("%s\n", befehle[i].argumente[j]);}
+		}
+	}
 }
 
 /*
-	Beachte!
+		Beachte!
 
-	2) Die Eingabezeile besteht aus beliebig vielen "Befehlen" (maximal 10,
-	moeglicherweise auch keinem), die jeweils durch ein Semikolon voneinander getrennt sind.
+		2) Die Eingabezeile besteht aus beliebig vielen "Befehlen" (maximal 10,
+		moeglicherweise auch keinem), die jeweils durch ein Semikolon voneinander getrennt sind.
 
-	3) Ein Befehl besteht aus "Worten" (maximal 20, moeglicherweise auch keinem):
-	ein Programmname, gefolgt von bis zu 19 Argumenten, die jeweils durch ein oder
-	mehrere Leerzeichen voneinander getrennt sind.
+		3) Ein Befehl besteht aus "Worten" (maximal 20, moeglicherweise auch keinem):
+		ein Programmname, gefolgt von bis zu 19 Argumenten, die jeweils durch ein oder
+		mehrere Leerzeichen voneinander getrennt sind.
 */
-void splitEingabe() {
-	/*//int weiche = 0;
-	int grenze = 0;
-	struct befehl temp;
-	int countSpaces = 0;
-	int j = 0;
-
-	// erst die Befehle zählen, wenn gleich ein dann bis zum Semikolon,
-	// ansonsten bis zum ersten Leerzeichen
-	// => Leerzeichen zählen
-	// 0 - nur progname
-	// > 0 mit argumente
-
-	for(int i=0; i < inputLength; i++){
-		if(eingabe[i] == ' ') {countSpaces++;}
-
-		if(eingabe[i] == ';') {
-
-			if(countSpaces == 0) {
-				while(eingabe[grenze] != ';') {
-					temp.programmname[j++] = eingabe[grenze++];
-					printf("yihaa\n");
-				}
-			}
-
-
-			// BSP: ls -al; ps; hurz 42; grep hallo test.c
-			// von grenze bis zum ersten leerzeichen
-
-
-			// von grenze bis zu i-1
-
-			// alles was vor der eingabe i steht wird an die Stelle 0 gepackt
-			// weiche um 1 erhöhen
-			// weiter bis zum nächsten, vorherige Stelle in einer Variablen merken
-		}
-	}*/
-}
-
-// Leerzeichen und leere Eingaben entfernen
-// ; hinten anhängen und vorne entfernen
-int prepareEingabe() {
-	eingabe[inputLength-1] = ';';
-	eingabe[inputLength] = '\n';
-
-	for(int i=0; i < inputLength; i++){
-		if(eingabe[i] == ' ' && eingabe[i+1] == ' ') {
-			memmove(&eingabe[i], &eingabe[i + 1], strlen(eingabe) - i);
-			i--;
-			inputLength--;
-		}
-	}
-
-	for(int i=0; i < inputLength; i++){
-		if(eingabe[i-1] == ';' && eingabe[i] == ' ') {
-			memmove(&eingabe[i], &eingabe[i + 1], strlen(eingabe) - i);
-			i--;
-			inputLength--;
-		}
-	}
-
-	if(eingabe[0] == ' ') {
-		memmove(&eingabe[0], &eingabe[0 + 1], strlen(eingabe) - 0);
-		inputLength--;
-	}
-
-	for(int i=0; i < inputLength; i++){
-		if(eingabe[i-1] == ';' && eingabe[i] == ';') {
-			memmove(&eingabe[i], &eingabe[i + 1], strlen(eingabe) - i);
-			i--;
-			inputLength--;
-		}
-	}
-
-	for(int i=0; i < inputLength; i++){
-		if(eingabe[i] == ' ' && eingabe[i+1] == ';') {
-			memmove(&eingabe[i], &eingabe[i + 1], strlen(eingabe) - i);
-			i--;
-			inputLength--;
-		}
-	}
-
-	if(eingabe[0] == ';' && eingabe[1] != '\n') {
-		memmove(&eingabe[0], &eingabe[0 + 1], strlen(eingabe) - 0);
-	}
-
-	int validiereEingabe = 0;
-	for(int i=0; i < inputLength; i++){
-		if(eingabe[i] == ';') {
-			validiereEingabe++;
-		}
-	}
-	if (validiereEingabe > 10) {
-		return -1;
-	}
-	return 0;
-}
--- progHUE01.h
+++ progHUE01.h
@@ -1,2 +1,1 @@
-void splitEingabe();
-int prepareEingabe();
+void test();
